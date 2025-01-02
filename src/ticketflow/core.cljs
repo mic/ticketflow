2cf2211296f388ec8bcec279cfa50a078cee900f
michal wlodkowski <mic@contextflow.com>
2025-01-02 17:14:34 +0100

    Add reagent

--- src/ticketflow/core.cljs
+++ src/ticketflow/core.cljs
@@ -1,4 +1,20 @@
-(ns ticketflow.core)
+(ns ticketflow.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as r.dom]
+            ["react-dom/client" :as ReactDOM]
+            [axios]
+            [moment]))
+
+(defonce root (ReactDOM/createRoot (.getElementById js/document "app")))
+
+;; (defn init []
+;;   (js/console.log "ClojureScript is working!"))
+
+(defn app []
+  [:div "Hello, React 18 with Reagent!"])
+
+(defn mount []
+  (.render root (r/as-element [app])))
 
 (defn init []
-  (js/console.log "ClojureScript is working!"))
+  (mount))
